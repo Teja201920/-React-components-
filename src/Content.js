320bc71ca6ee3aa4585846a166dec5741c2646f1
Teja201920 <darlingteja2019@gmail.com>
2025-09-17 14:25:56 +0530

    first commit

---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main>
+      <p>This is the main content section of my React app.</p>
+    </main>
+  );
+}
+
+export default Content;
